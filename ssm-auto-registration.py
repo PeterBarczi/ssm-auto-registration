7c4700a0872cec9b41bd4f30005057001177d492
Pety <35726295+PeterBarczi@users.noreply.github.com>
2021-08-30 14:17:08 +0200

    Update ssm-auto-registration.py

--- ssm-auto-registration.py
+++ ssm-auto-registration.py
@@ -11,24 +11,81 @@ def lambda_handler(event, context):
     # Activation
     resp = client.create_activation(
            #Description = socket.getfqdn(),
-            Description = 'TestingActivation',
+            Description = 'MyActivation',
             DefaultInstanceName = 'test',
             IamRole = 'service-role/AmazonEC2RunCommandRoleForManagedInstances',
-            RegistrationLimit = 100,
+            RegistrationLimit = 1,
             Tags=[
                 {
                     'Key': 'Customer',
-                    'Value': 'X'
+                    'Value': 'XYZ'
                 },
             ]
         )   
-    # Stores Output into file
-    with open('/tmp/reg-creds', 'w') as f:
-        print(resp, file=f)
+    # I. Stores Output into file
+    #with open('/tmp/reg-creds', 'w') as f:
+    #    print(resp, file=f)
     #
-    # Print Output
-    print(resp)
+    # II. Print Output
+    #print(resp)
     #
-    # Upload output file to S3
-    s3 = boto3.resource(service_name = 's3')
-    s3.meta.client.upload_file(Filename = '/tmp/reg-creds', Bucket = 'tsy-vmc-dev-s3-euc1-test', Key = 'reg-creds')
+    # III. Upload output file to S3
+    #s3 = boto3.resource(service_name = 's3')
+    #s3.meta.client.upload_file(Filename = '/tmp/reg-creds', Bucket = 'tsy-vmc-dev-s3-euc1-test', Key = 'reg-creds')
+    #
+    # IV. Parse Output directly
+    id = resp.get("ActivationId")
+    code = resp.get("ActivationCode")
+    print(id)
+    print(code)
+    
+    # Part II
+    client = boto3.client('route53')
+    
+    response = client.change_resource_record_sets(
+    HostedZoneId='Z0797032DCH5FW0MZB7P',
+    ChangeBatch={
+        'Comment': 'Add entries',
+        'Changes': [
+            {
+                'Action': 'UPSERT',
+                'ResourceRecordSet': {
+                    'Name': 'code.big-data.sk.',
+                    'ResourceRecords': [
+                        {
+                            'Value': "\"5Pib5OW9nBFs57ddVqvLkzx\"",
+                        },
+                    ],
+                    'TTL': 60,
+                    'Type': 'TXT',
+                },
+            },
+            {
+                'Action': 'UPSERT',
+                'ResourceRecordSet': {
+                    'Name': 'id.big-data.sk.',
+                    'ResourceRecords': [
+                        {
+                            'Value': "\"new\"",
+                        },
+                    ],
+                    'TTL': 60,
+                    'Type': 'TXT',
+                },
+            },
+            {
+                'Action': 'UPSERT',
+                'ResourceRecordSet': {
+                    'Name': 'region.big-data.sk.',
+                    'ResourceRecords': [
+                        {
+                            'Value': "\"myregion\"",
+                        },
+                    ],
+                    'TTL': 60,
+                    'Type': 'TXT',
+                },
+            },
+        ],
+    },
+    )
